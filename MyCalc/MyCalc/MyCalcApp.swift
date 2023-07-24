6c1d3d110ff1963ac5e454afdd0da1803e1fb3f8
Yurii Chukhlib <yuri.v.chu@gmail.com>
2023-07-24 12:34:15 +0400

    Project created.

---
+++ MyCalc/MyCalc/MyCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalcApp.swift
+//  MyCalc
+//
+//  Created by Yuri Chukhlib on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
